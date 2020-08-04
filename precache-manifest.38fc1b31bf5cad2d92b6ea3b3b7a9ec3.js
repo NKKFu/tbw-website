2d67b13bd2963be94b31986ad528f67b07f91210
nkkf <nelsonkenmochi@gmail.com>
2020-08-03 21:01:57 -0400

    Updates

---
+++ precache-manifest.38fc1b31bf5cad2d92b6ea3b3b7a9ec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce1268a965735afd0cf6ac082660308",
+    "url": "/tbw-website/index.html"
+  },
+  {
+    "revision": "63c370137f83dc8f1afe",
+    "url": "/tbw-website/static/js/2.82e0e6f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tbw-website/static/js/2.82e0e6f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64b31d4078fd8f42555",
+    "url": "/tbw-website/static/js/main.c2bcb5f6.chunk.js"
+  },
+  {
+    "revision": "7cb269c982d56421f68a",
+    "url": "/tbw-website/static/js/runtime-main.b9100e47.js"
+  }
+]);
